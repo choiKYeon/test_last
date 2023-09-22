e49130de19cf156a480a0d425df8c285cc445537
ckh <gusrudchl12@gmail.com>
2023-09-22 12:22:14 +0900

    오늘

--- src/main/java/org/example/car.java
+++ src/main/java/org/example/car.java
@@ -1,11 +1,37 @@
 package org.example;
 
-class 무기 {
+class 전사 {
+    무기 a왼손무기;
+    무기 a오른손무기;
+
+
     void 공격() {
-        System.out.println("공격 조진다");
+        if (a왼손무기 != null) {
+            a왼손무기.실행();
+        }else if(a오른손무기 != null){
+            a오른손무기.실행();
+        }
     }
 }
 
+class 무기 {
+    void 실행() {
+    }
+}
 class 칼 extends 무기 {
+    void 실행() {
+        System.out.println("전사가 왼손으로 칼(을)를 사용합니다.");
+    }
+}
 
+class 창 extends 무기 {
+    void 실행() {
+        System.out.println("전사가 왼손으로 창(을)를 사용합니다.");
+    }
+}
+
+class 도끼 extends 무기 {
+    void 실행() {
+        System.out.println("전사가 오른손으로 도끼(을)를 사용합니다.");
+    }
 }
