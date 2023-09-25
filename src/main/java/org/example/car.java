b6a38e592f08507275b026e3969232ef137a3204
ckh <gusrudchl12@gmail.com>
2023-09-25 13:41:31 +0900

    오 늘

--- src/main/java/org/example/car.java
+++ src/main/java/org/example/car.java
@@ -1,37 +1,44 @@
 package org.example;
 
+
+/* 구현 시작 */
 class 전사 {
-    무기 a왼손무기;
-    무기 a오른손무기;
-
-
-    void 공격() {
-        if (a왼손무기 != null) {
-            a왼손무기.실행();
-        }else if(a오른손무기 != null){
-            a오른손무기.실행();
-        }
+    무기 a무기;
+    void 실행(){
+        a무기.공격();
     }
 }
 
-class 무기 {
-    void 실행() {
+class 무기{
+    String 이름;
+    int 번호;
+    void 공격(){
+        System.out.println(this.이름 + "(으)로 공격합니다.");
+        System.out.println("데미지 :" + this.번호);
     }
 }
-class 칼 extends 무기 {
-    void 실행() {
-        System.out.println("전사가 왼손으로 칼(을)를 사용합니다.");
+class 칼 extends 무기{
+    칼() {
+        this.이름 = "칼";
+        this.번호 = 78;
     }
 }
-
-class 창 extends 무기 {
-    void 실행() {
-        System.out.println("전사가 왼손으로 창(을)를 사용합니다.");
+class 창 extends 무기{
+    창() {
+        this.이름 = "창";
+        this.번호 = 80;
     }
 }
-
-class 도끼 extends 무기 {
-    void 실행() {
-        System.out.println("전사가 오른손으로 도끼(을)를 사용합니다.");
+class 지팡이 extends 무기{
+    지팡이() {
+        this.이름 = "지팡이";
+        this.번호 = 12;
     }
 }
+class 전설의_지팡이 extends 무기{
+    전설의_지팡이() {
+        this.이름 = "전설의_지팡이";
+        this.번호 = 120;
+    }
+}
+/* 구현 끝 */
