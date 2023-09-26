a4b17dafe551a772c970d061856391665469cdf4
ckh <gusrudchl12@gmail.com>
2023-09-26 10:49:41 +0900

    human

--- src/main/java/org/example/car.java
+++ src/main/java/org/example/car.java
@@ -1,57 +1,10 @@
 package org.example;
 
-abstract class 오리 {
-    비행아이템 a비행아이템;
-
-    오리() {
-        a비행아이템 = new 날개비행아이템();
+class 사람{
+    사람(){
+        System.out.println("사람이 태어났습니다.");
     }
-
-    void 날다() {
-        a비행아이템.작동();
-    }
-}
-
-class 흰오리 extends 오리 {
-
-}
-
-class 청둥오리 extends 오리 {
-
-}
-
-class 고무오리 extends 오리 {
-    고무오리() {
-        a비행아이템 = new 못나는비행아이템();
-    }
-    void 더날다(){
-        a비행아이템 = new 로켓비행아이템();
-    }
-}
-
-class 고무2오리 extends 오리 {
-    고무2오리() {
-        a비행아이템 = new 못나는비행아이템();
-    }
-}
-
-abstract class 비행아이템 {
-    abstract void 작동();
-}
-
-class 날개비행아이템 extends 비행아이템 {
-    void 작동() {
-        System.out.println("날개로 날아갑니다.");
-    }
-}
-
-class 못나는비행아이템 extends 비행아이템 {
-    void 작동() {
-        System.out.println("저는 날 수 없어요. ㅠㅠ");
-    }
-}
-class 로켓비행아이템 extends 비행아이템{
-    void 작동(){
-        System.out.println("로켓파워로 날아갑니다.");
+    사람(int a){
+        System.out.println("태어나서부터" + a + "살인 사람이 태어났습니다.");
     }
 }
