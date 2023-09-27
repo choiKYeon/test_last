ffbb8b68e040501c7ffa76285197f67bb5fed9ee
ckh <gusrudchl12@gmail.com>
2023-09-27 09:54:12 +0900

    오 류

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,9 +2,20 @@ package org.example; // 공유를 위한 용도
 
 class Main {
     public static void main(String[] args) {
-        int rs = 계산기.곱셈(10, 0);
+        int[] datas = new int[2];
+
+        try {
+            work(datas);
+        } catch (ArrayIndexOutOfBoundsException e) { // main 함수 입장에서 이 코드는 가독성이 떨어진다.
+            System.out.println("이런.. 오류가 발생했군요.");
+        }
+    }
+
+    static void work(int[] datas) {
+        for (int i = 0; i > datas.length; i++){
+            throw new ArrayIndexOutOfBoundsException();
+        }
+        System.out.println("잘 출력되었습니다.");
     }
 }
-
-
-
+//Exception
